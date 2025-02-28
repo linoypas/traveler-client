2843a2a5cccfe28df711e690f2ab71b400a88f07
לינוי פסינקוב <linoypasinkov@gmail.com>
2025-02-28 22:30:22 +0200

    auth components

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Login from './components/login';
-import Register from './components/register';
+import Login from './components/auth/login';
+import Register from './components/auth/register';
 
 function App() {
   return (
