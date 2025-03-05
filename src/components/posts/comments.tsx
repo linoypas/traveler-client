f6be76946296cfc8e49425541ee931cf364dffde
לינוי פסינקוב <linoypasinkov@gmail.com>
2025-03-05 15:51:16 +0200

    option to upload both image and content

--- src/components/posts/comments.tsx
+++ src/components/posts/comments.tsx
@@ -22,31 +22,12 @@ const Post = () => {
     const [comments, setComments] = useState<Comment[]>([]);
     const [post, setPost] = useState<IPost | null>(null);
     const [loading, setLoading] = useState(true);
-    const [isLiked, setIsLiked] = useState(false);
-    const [likes, setLikes] = useState(0);
 
     const userId = localStorage.getItem('id'); 
     const accessToken = localStorage.getItem('accessToken');
     useEffect(() => {
         const fetchPost = async () => {
           try {
-            const postReponse = await fetch(`http://localhost:3001/posts/${postId}`,{
-                method: 'GET',
-                headers: {
-                  'Authorization': `Bearer ${accessToken}`,
-                  'Content-Type': 'application/json',
-                },
-              });
-            
-            if (!postReponse.ok) {
-              throw new Error('Failed to fetch post');
-            }
-            const postData = await postReponse.json();
-            setPost(postData);
-            setLikes(postData.likes.length);
-            if(postData.likes.includes(userId)){
-                setIsLiked(true);
-            }
             const commentResponse = await fetch(`http://localhost:3001/comments?postId=${postId}`, {
                 method: "GET",
                 headers: {
@@ -90,58 +71,26 @@ const Post = () => {
                 console.error("Error posting comment", error);
             };
         };
-    const handleLike = async (e:any) => { 
-        e.preventDefault();
-        if(!post || !userId)    
-            return;
-        let updatedLikes= [...post.likes];
-        if(isLiked){
-            updatedLikes= updatedLikes.filter(id => id !== userId);
-        } else {
-            updatedLikes.push(userId) ;
-        }
-        try {
-            const response = await fetch(`http://localhost:3001/posts/${postId}`, {
-                method: "PUT",
-                headers: {
-                    'Authorization': `Bearer ${accessToken}`,
-                    "Content-Type": "application/json" },
-                body: JSON.stringify({ likes: updatedLikes}),
-            });
-            if (!response.ok) {
-                console.error("Failed to update likes");
-            }
-            setIsLiked(!isLiked);
-            setLikes(updatedLikes.length);
-            setPost({ ...post, likes: updatedLikes });
-
-        } catch (error) {
-            console.error("Error updating likes", error);
-        }
-    }
-
-    const isImage = (content: string) => {
-        return /\.(jpeg|jpg|png|gif|webp)$/i.test(content);
-    };
-
+  
     if (!post || loading) {
         return <p>Loading...</p>; 
     }
     return (
     <div>
         <h3>{post.title}</h3>
-        <p><strong>By: {post.owner || 'Unknown'}</strong></p> 
-        {isImage(post.content) ? (
-                <img src={post.content} alt="Post content" style={{ width: '100%', height: 'auto' }} />
-            ) : (
-                <p>{post.content}</p>
-            )}        <button onClick={handleLike}>
-            {isLiked ? 'Unlike' : 'Like'} ({likes} Likes)
-      </button>
-      <div>
-        <h4>Comments:</h4> {comments.length} 
+        <div>
+            <h4>Comments:</h4>
+            {comments.length > 0 ? (
+                    comments.map((comment, idx) => (
+                        <div key={idx}>
+                            <strong>{comment.owner}</strong>: {comment.content}
+                        </div>
+                    ))
+                ) : (
+                    <p>No comments yet</p>
+                )}
         </div>
-        </div>
+    </div>
     );
 }
 
