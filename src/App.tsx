9b5c0d3ea37cd82e07ba08a571321d30af5aa7a2
לינוי פסינקוב <linoypasinkov@gmail.com>
2025-03-07 17:17:37 +0200

    add option to delete and edit comments and posts

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,8 @@ import Header from './components/header';
 import Comments from './components/posts/comments';
 import AiPosts from './components/posts/AIPosts';
 import Posts from './components/posts/posts';
+import EditPost from './components/posts/editPost';
+
 function App() {
   return (
     <Router>
@@ -19,6 +21,7 @@ function App() {
         <Route path="/posts" element={<Posts />} />
         <Route path="/comments/:postId" element={<Comments />} />
         <Route path="/posts/:postId" element={<Post />} />
+        <Route path="/posts/edit/:postId" element={<EditPost />} />
         <Route path="/ai-posts" element={<AiPosts />} />
 
 
