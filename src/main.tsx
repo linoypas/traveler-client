4b9b6ad67fa347a09fe896a16f3d4c3751204d32
linoypas <linoypasinkov@gmail.com>
2025-03-22 21:56:19 +0200

    add option to login with google

--- src/main.tsx
+++ src/main.tsx
@@ -4,9 +4,7 @@ import './index.css'
 import App from './App.tsx'
 import { GoogleOAuthProvider } from '@react-oauth/google';
 
-const CLIENT_ID = '221317044226-c72ffb74ljd9ehh77fahkgm3g61nka85.apps.googleusercontent.com';
-
-
+const CLIENT_ID = import.meta.env.VITE_GOOGLE_CLIENT_ID;
 createRoot(document.getElementById('root')!).render(
   <GoogleOAuthProvider clientId={CLIENT_ID}>
     <App />
