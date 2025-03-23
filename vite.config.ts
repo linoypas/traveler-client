15fcefb2dcc6cd40b3fded735ea075d464cde0c5
Noa Ben zion <67430185+noa1608@users.noreply.github.com>
2025-03-23 13:21:31 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: 'localhost',
-    port: 5173, // Change port if needed
+    host: '0.0.0.0',
+    port: 4000, // Change port if needed
   },
 })
