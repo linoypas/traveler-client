0d93fa202aa9d932342c9f0ddb6f63ff8bec17ac
linoypas <linoypasinkov@gmail.com>
2025-03-23 03:49:35 +0200

    fix package.json

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
 import Login from './components/auth/login';
 import Register from './components/auth/register';
